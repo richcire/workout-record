5e55fdd8677f3abde6d2ccb0b5e6b75e09c516a1
richcire <eric9907@naver.com>
2022-06-18 19:01:02 +0900

    apply framer motion to side bar

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -3,6 +3,18 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { GoCalendar } from "react-icons/go";
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+const NumberOfDays = styled.div`
+  font-size: 8rem;
+  font-family: "Roboto Slab", serif;
+  font-weight: bold;
+  color: #341f97;
+  width: 100%;
+  text-align: center;
+  position: relative;
+  top: 5%;
+`;
 
 const Window = styled.div`
   height: 100vh;
@@ -16,35 +28,32 @@ const Window = styled.div`
   }
 `;
 
-const SideBarContainer = styled.div<{ isSideBarVisible: boolean }>`
-  position: fixed;
-  display: ${(props) => (props.isSideBarVisible ? "flex" : "none")};
-  top: 0;
-  height: 100vh;
-  width: 100vw;
-`;
+// const SideBarContainer = styled.div<{ isSideBarVisible: boolean }>`
+//   position: fixed;
+//   display: ${(props) => (props.isSideBarVisible ? "flex" : "none")};
+//   top: 0;
+//   height: 100vh;
+//   width: 100vw;
+// `;
 
-const Overlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.3);
-  width: 80%;
-  height: 100%;
-`;
+// const Overlay = styled.div`
+//   background-color: rgba(0, 0, 0, 0.3);
+//   width: 100%;
+//   height: 100%;
+// `;
 
-const SideBar = styled.div`
-  right: 0;
-  top: 0;
+const SideBar = styled(motion.div)`
   background-color: whitesmoke;
+  position: fixed;
+  top: 0;
+  right: 0;
   height: 100%;
-  width: 350px;
 `;
 
-const NumberOfDays = styled.div`
-  font-family: "Roboto Slab", serif;
-  font-weight: bold;
-  color: #341f97;
-  width: 100%;
-  text-align: center;
-`;
+const variants = {
+  open: { opacity: 1, x: 0 },
+  close: { opacity: 0, x: "-100%" },
+};
 
 function MainPage() {
   const [isSideBarVisible, setIsSideBarVisible] = useState(false);
@@ -56,14 +65,15 @@ function MainPage() {
 
   return (
     <Window>
-      <NumberOfDays>number of days</NumberOfDays>
+      <NumberOfDays>365</NumberOfDays>
       <GoCalendar className="calendar" size={50} onClick={onIconClick} />
-      <SideBarContainer isSideBarVisible={isSideBarVisible}>
-        <Overlay onClick={onIconClick} />
-        <SideBar>
-          <Calendar />
-        </SideBar>
-      </SideBarContainer>
+
+      <SideBar
+        animate={isSideBarVisible ? "open" : "close"}
+        variants={variants}
+      >
+        <Calendar />
+      </SideBar>
     </Window>
   );
 }
