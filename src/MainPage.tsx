a77498a2c13f0fa7c8e5b70c5bbeaaf1e39ef7bf
richcire <eric9907@naver.com>
2022-06-18 18:15:45 +0900

    create side bar

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,9 +1,41 @@
 import styled from "styled-components";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { GoCalendar } from "react-icons/go";
+import { useState } from "react";
 
 const Window = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: #a0eeff;
+
+  .calendar {
+    position: absolute;
+    top: 20px;
+    right: 2%;
+  }
+`;
+
+const SideBarContainer = styled.div<{ isSideBarVisible: boolean }>`
+  position: fixed;
+  display: ${(props) => (props.isSideBarVisible ? "flex" : "none")};
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 80%;
+  height: 100%;
+`;
+
+const SideBar = styled.div`
+  right: 0;
+  top: 0;
+  background-color: whitesmoke;
+  height: 100%;
+  width: 350px;
 `;
 
 const NumberOfDays = styled.div`
@@ -15,9 +47,23 @@ const NumberOfDays = styled.div`
 `;
 
 function MainPage() {
+  const [isSideBarVisible, setIsSideBarVisible] = useState(false);
+
+  const onIconClick = () => {
+    setIsSideBarVisible((current) => !current);
+    console.log(isSideBarVisible);
+  };
+
   return (
     <Window>
       <NumberOfDays>number of days</NumberOfDays>
+      <GoCalendar className="calendar" size={50} onClick={onIconClick} />
+      <SideBarContainer isSideBarVisible={isSideBarVisible}>
+        <Overlay onClick={onIconClick} />
+        <SideBar>
+          <Calendar />
+        </SideBar>
+      </SideBarContainer>
     </Window>
   );
 }
