c661bc12f756dd887b2968ee2f912f83ec228590
richcire <eric9907@naver.com>
2022-06-20 13:33:54 +0900

    finish sidebar animation

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -30,18 +30,17 @@ const SideBar = styled(motion.div)`
 
   height: 100vh;
   width: 400px;
-  border-top-left-radius: 80px;
-  border-bottom-left-radius: 100px;
-  /* clip-path: circle(500px at 340px 60px); */
+  border-top-left-radius: 40px;
+  border-bottom-left-radius: 50px;
 `;
 
-const SideBarBtn = styled.button`
+const BtnContainer = styled.button`
   position: absolute;
   top: 30px;
   left: 310px;
   width: 60px;
   height: 60px;
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: transparent;
   border-radius: 50%;
   border: none;
   padding: 0;
@@ -58,6 +57,51 @@ const sideBarVariants = {
   },
 };
 
+interface IProps {
+  variants: {
+    closed: { d?: string; opacity?: number };
+    open: { d?: string; opacity?: number };
+  };
+  d?: string;
+  transition?: { duration: number };
+}
+const Path = (props: IProps) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="hsl(0, 0%, 18%)"
+    strokeLinecap="round"
+    {...props}
+  />
+);
+
+const SideBarBtn = () => (
+  <BtnContainer>
+    <svg width={23} height={23} viewBox="0 0 23 23">
+      <Path
+        variants={{
+          closed: { d: "M 2 2.5 L 20 2.5" },
+          open: { d: "M 3 16.5 L 17 2.5" },
+        }}
+      />
+      <Path
+        d="M 2 9.423 L 20 9.423"
+        variants={{
+          closed: { opacity: 1 },
+          open: { opacity: 0 },
+        }}
+        transition={{ duration: 0.1 }}
+      />
+      <Path
+        variants={{
+          closed: { d: "M 2 16.346 L 20 16.346" },
+          open: { d: "M 3 2.5 L 17 16.346" },
+        }}
+      />
+    </svg>
+  </BtnContainer>
+);
+
 function MainPage() {
   const [isOpen, cycleIsOpen] = useCycle(false, true);
 
@@ -69,7 +113,30 @@ function MainPage() {
         animate={isOpen ? "open" : "closed"}
         variants={sideBarVariants}
       >
-        <SideBarBtn onClick={() => cycleIsOpen()} />
+        <BtnContainer onClick={() => cycleIsOpen()}>
+          <svg width={23} height={23} viewBox="0 0 23 23">
+            <Path
+              variants={{
+                closed: { d: "M 2 2.5 L 20 2.5" },
+                open: { d: "M 3 16.5 L 17 2.5" },
+              }}
+            />
+            <Path
+              d="M 2 9.423 L 20 9.423"
+              variants={{
+                closed: { opacity: 1 },
+                open: { opacity: 0 },
+              }}
+              transition={{ duration: 0.1 }}
+            />
+            <Path
+              variants={{
+                closed: { d: "M 2 16.346 L 20 16.346" },
+                open: { d: "M 3 2.5 L 17 16.346" },
+              }}
+            />
+          </svg>
+        </BtnContainer>
       </SideBar>
     </Window>
   );
