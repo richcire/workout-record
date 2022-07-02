6b2ecda508cbdeed81b069cb531ac28eab9a4116
richcire <eric9907@naver.com>
2022-07-02 13:47:30 +0900

    update chartData base on data2022

--- src/components/ChartSection.tsx
+++ src/components/ChartSection.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { MonthView } from "react-calendar";
 import { useRecoilValue } from "recoil";
@@ -26,16 +26,35 @@ const ChartOptionBtn = styled.button``;
 const onTotalBtnClickedUponMonth = (
   data2022: IData2022,
   chartCategoriesMonth: string[]
-) => {
+): number[] => {
   const chartData: number[] = [];
   const existingMonthsArray = Object.keys(data2022);
 
   for (const month of chartCategoriesMonth) {
     if (existingMonthsArray.includes(month)) {
-      let average = 0;
-      console.log(data2022[month]);
+      const datesToIntList = Object.keys(data2022[month]).map((data) =>
+        parseInt(data)
+      );
+
+      let recentDate: number | string = Math.max(...datesToIntList);
+      console.log(recentDate);
+      // 1 != "01"
+      if (recentDate.toString().length === 1) {
+        recentDate = "0" + recentDate;
+      }
+      const recentDateData = data2022[month][recentDate];
+      console.log(recentDateData);
+      chartData.push(
+        parseInt(recentDateData.benchPress) +
+          parseInt(recentDateData.squat) +
+          parseInt(recentDateData.deadlift)
+      );
+    } else {
+      chartData.push(0);
     }
   }
+  console.log(chartData);
+  return chartData;
 };
 
 // const makeMonthChartData = (
@@ -69,14 +88,24 @@ const makeDateChartData = (dataType: string, chartCategories: string[]) => {};
 
 function ChartSection() {
   const data2022 = useRecoilValue(data2022State);
+
   const currentMonthIdx = new Date().getMonth();
   const chartCategoriesMonth = makeCategoriesMonth(currentMonthIdx);
   const chartCategoriesDate = makeCategoriesDate(data2022);
 
+  useEffect(() => {
+    setChartData(onTotalBtnClickedUponMonth(data2022, chartCategoriesMonth));
+  }, [data2022]);
+
   const [chartCategories, setChartCategories] =
     useState<string[]>(chartCategoriesMonth);
 
-  const [chartData, setChartData] = useState<number[]>();
+  const [chartData, setChartData] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const onTotalBtnClicked = (data2022: IData2022 | undefined) => {
+    if (typeof data2022 === "undefined") {
+    }
+  };
 
   const options = {
     chart: {
@@ -89,7 +118,7 @@ function ChartSection() {
   const series = [
     {
       name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
+      data: chartData,
     },
   ];
 
